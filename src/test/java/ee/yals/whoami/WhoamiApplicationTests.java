268b8eb3345585133bea95267fa493c27763092a
Alex Muravya <kyberorg@yadev.eu>
2019-08-22 11:35:03 +0000

    Tests

--- src/test/java/ee/yals/whoami/WhoamiApplicationTests.java
+++ src/test/java/ee/yals/whoami/WhoamiApplicationTests.java
@@ -3,9 +3,14 @@ package ee.yals.whoami;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
+@ContextConfiguration({"classpath*:test-app.xml"})
+@TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest
 public class WhoamiApplicationTests {
 
